741c62e92fe3f487caf8b8c746a4515b78ad7db8
MorelSami <mksami237@gmail.com>
2024-04-30 20:01:36 -0400

    terraform backend update

--- providers.tf
+++ providers.tf
@@ -1,21 +1,19 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-#terraform {
-#  required_providers {
-#    azurerm = {
-#      source  = "hashicorp/azurerm"
-#      version = "=3.0.0"
-#    }
-
-#  }
-#backend "azurerm" {
-#      resource_group_name  = "remote"
-#      storage_account_name = "boboguguruyole"
-#      container_name       = "containertfstate"
-#      key                  = "terraform.tfstate"
-#  }
-
-#}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+/*backend "azurerm" {
+      resource_group_name  = "tfstate-rg"
+      storage_account_name = "smorel"
+      container_name       = "remote-tfstate"
+      key                  = "terraform.tfstate"
+  }*/
+}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
--- remote.sh
+++ remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RESOURCE_GROUP_NAME=tfstate-rg
-STORAGE_ACCOUNT_NAME=smorel-$RANDOM
+STORAGE_ACCOUNT_NAME=smorel$RANDOM
 CONTAINER_NAME=remote-tfstate
 
 # Create resource group
@@ -11,4 +11,4 @@ az group create --name $RESOURCE_GROUP_NAME --location eastus
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME #
